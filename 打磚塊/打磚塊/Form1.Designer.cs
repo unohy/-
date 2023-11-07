d95bd54274e651a69c7f126983d2b5aba3c3af6b
unohy <150133563+unohy@users.noreply.github.com>
2023-11-07 18:33:34 +0800

    step1

---
+++ 打磚塊/打磚塊/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace 打磚塊
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox_ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_ball
+            // 
+            this.pictureBox_ball.Image = global::打磚塊.Properties.Resources.fitness_ball1;
+            this.pictureBox_ball.Location = new System.Drawing.Point(113, 354);
+            this.pictureBox_ball.Name = "pictureBox_ball";
+            this.pictureBox_ball.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox_ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_ball.TabIndex = 0;
+            this.pictureBox_ball.TabStop = false;
+            this.pictureBox_ball.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(855, 493);
+            this.Controls.Add(this.pictureBox_ball);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_ball;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 打磚塊/打磚塊/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 打磚塊
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int dx=30, dy=30;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox_ball.Left += dx;
+            pictureBox_ball.Top += dy;
+
+            if (pictureBox_ball.Top >= this.Height - pictureBox_ball.Height)
+            {
+                //pictureBox_ball.Left += 50;
+                //pictureBox_ball.Top -= 50;
+                dy *= -1;
+            }
+            else if (pictureBox_ball.Left >= this.Width - pictureBox_ball.Width)
+            {
+                //pictureBox_ball.Left -= 50;
+                //pictureBox_ball.Top -= 50;
+                dx *= -1;
+            }
+            else if (pictureBox_ball.Top <= 0)
+            {
+                //pictureBox_ball.Left -= 50;
+                //pictureBox_ball.Top += 50;
+                dy *= -1;
+            }
+            else if (pictureBox_ball.Left <= 0)
+            {
+                //pictureBox_ball.Left += 50;
+                //pictureBox_ball.Top += 50;
+                dx *= -1;
+            }
+            else
+            {
+                //pictureBox_ball.Left += 50;
+                //pictureBox_ball.Top += 50;
+            }
+            
+        }
+    }
+}
